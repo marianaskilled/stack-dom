4fda6a3f13fde05a50fae3371ce2485aee2e6800
marianaskilled <marianaskilled@gmail.com>
2022-07-17 22:16:28 +0300

    remove first childe

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let insertPoint = document.getElementById('stack');
 let num = document.getElementById('num');
 
-
 function push( ) {
     let box = document.createElement('div');
     box.setAttribute('class', 'box');
@@ -12,5 +11,5 @@ function push( ) {
 
 function pop( ) {
     let insertPoint2 = document.querySelector('.stack');
-    insertPoint2.removeChild(insertPoint2.firstChild);
+    insertPoint2.removeChild(insertPoint2.lastChild);
 }
