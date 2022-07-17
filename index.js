7af1060dabdb8b85a11cd30ca52c43afcf5b66c5
marianaskilled <marianaskilled@gmail.com>
2022-07-17 19:48:12 +0300

    stack dom

---
+++ index.js
@@ -0,0 +1,15 @@
+let insertPoint = document.getElementById('stack');
+let num = document.getElementById('num');
+
+
+function push( ) {
+    let box = document.createElement('div');
+    box.setAttribute('class', 'box');
+    box.innerText = num.value;
+    insertPoint.appendChild(box);
+
+}
+
+function pop( ) {
+        insertPoint.re();
+}
